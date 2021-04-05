ae6e4cacd3383db6d95787cb7c48d4b4a156d639
Lior Friedman <lior.friedman@similarweb.com>
2021-04-05 20:06:57 +0300

    commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dtuslpttrvdgck:8061616320cdebba6157862b3ee490506ac39f05f75491115cca3f84beaa1c0d@ec2-54-73-68-39.eu-west-1.compute.amazonaws.com:5432/d6jfkoshrj9dkl'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'lior'
 api = Api(app)
