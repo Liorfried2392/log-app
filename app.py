da35820a8ef2b079aa74474180535aeb4c04eb9b
Lior Friedman <lior.friedman@similarweb.com>
2021-07-14 18:21:05 +0300

    "fix"

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI','sqlite:///data.db')
+db_url = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'lior'
 api = Api(app)
